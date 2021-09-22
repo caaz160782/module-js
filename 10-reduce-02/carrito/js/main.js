90649494c85b3ec63330c5a806509089bcd17648
Carlos Aguirre <caaz160782@gmail.com>
2021-09-21 23:27:25 -0500

    ejercicios reduce

---
+++ 10-reduce-02/carrito/js/main.js
@@ -0,0 +1,67 @@
+const cartShoppings = [
+    {
+        name: 'Dip Tostitos Queso Suave 255 g',
+        description: 'Dip Tostitos Queso Suave 255 g',
+        price: 69.3,
+        type: 'chip'
+    },
+    {
+        name: 'Dip Tostitos Queso Suave 255 g',
+        description: 'Dip Tostitos Queso Suave 255 g',
+        price: 69.3,
+        type: 'chip'
+    },
+    {
+        name: 'Boing Mango 500ml',
+        description: 'Boing Mango 500ml',
+        price: 10,
+        type: 'drink'
+    },
+    {
+        name: 'Boing Mango 500ml',
+        description: 'Boing Mango 500ml',
+        price: 10,
+        type: 'drink'
+    }
+]
+
+
+// Obtener el importe total a pagar del carrito de los productos selecionados
+// nombre prodcuto                | cantidad | importe
+// Dip Tostitos Queso Suave 255 g |    2     |  138.3
+// Boing Mango 500ml              |    2     |  20
+// total                                     |   158.3
+const ticket =(arrayCartShop) =>{
+   //chip
+   let Chip= arrayCartShop.filter(( products) => products.type ==="chip")
+   let totalChip= Chip.map(( priceChip) => priceChip.price) 
+   let sumaChip = totalChip.reduce((accum, number)=> {
+    return accum + number
+    }, 0)    
+   //drink
+    let drink= arrayCartShop.filter(( products) => products.type ==="drink")
+    let totalDrink= drink.map(( priceDrink) => priceDrink.price) 
+    let sumaDrink = totalDrink.reduce((accum, number)=> {
+    return accum + number
+    }, 0)   
+    ///total
+    let total= arrayCartShop.map(( price) => price.price) 
+    let sumaTotal = total.reduce((accum, number)=> {
+    return accum + number
+    }, 0)   
+    //console.log(sumaTotal);
+  
+    const result = arrayCartShop.reduce((accum, product, index)=>{
+       //accum[product.name]= accum[product.name] ? accum[product.name] + 1 : 1        
+       accum[product.name]= accum[product.name] ? accum[product.name] + 1 : 1        
+       // console.log(accum[product.name])
+        
+        return {
+            ...accum,
+            [product.importe]: sumaChip
+        }
+     }, {})    
+    return result
+}
+console.log( ticket(cartShoppings) )
+
---
+++ 10-reduce-02/votar/js/main.js
@@ -0,0 +1,178 @@
+let arrayPersons = [
+    {
+        name: "Alice", age: 25, voted: true, gender: "mujer"
+    },
+    {
+        name: "Jenifer", age: 26, voted: false, gender: "mujer"
+    },
+    {
+        name: "Bruno", age: 48, voted: false, gender: "hombre"
+    },
+    {
+        name: "Jon", age: 22, voted: true, gender: "hombre"
+    },
+    {
+        name: "Brian", age: 20, voted: false, gender: "hombre"
+    },
+    {
+        name: "Luis", age: 38, voted: true, gender: "hombre"
+    },
+    {
+        name: "David", age: 29, voted: true, gender: "hombre"
+    },
+    {
+        name: "Ximena", age: 40, voted: false, gender: "mujer"
+    },
+    {
+        name: "Paulet", age: 37, voted: false, gender: "mujer"
+    },
+    {
+        name: "Albert", age: 45, voted: true, gender: "hombre"
+    },
+    {
+        name: "Mike", age: 22, voted: false, gender: "hombre"
+    },
+    {
+        name: "Tami", age: 35, voted: true, gender: "hombre"
+    },
+    {
+        name: "Mari", age: 28, voted: true, gender: "mujer"
+    },
+    {
+        name: "Mireya", age: 18, voted: true, gender: "mujer"
+    },
+    {
+        name: "Tamara", age: 18, voted: true, gender: "mujer"
+    },
+]
+
+/*
+Usar reduce para obtener la siguiente informacion
+- Un array con aquellas personas que votaron
+- la edad promedio de todos los votantes
+- un array con aquellos votantes que votaron y que son menores a 30 años
+- la edad promedio de los no votantes
+
+- el porcentaje de votantes vs no votante
+
+*/
+
+console.log("-----1----")
+
+const vote =(arrayPersons) =>{
+   const votaron = arrayPersons.reduce( (accum,persona) => { 
+             return persona.voted  
+                    ?[...accum, persona]        
+                    :accum 
+    },[])
+    return votaron
+}
+
+console.log(vote(arrayPersons))
+let votaron=vote(arrayPersons)
+
+console.log("-----2----")
+
+const edadPromedio =(arrayPersons) =>{    
+    const sumaEdad = arrayPersons.reduce( (accum,persona) => { 
+            return accum + persona.age      
+    },0)
+    return sumaEdad/arrayPersons.length
+}
+console.log(edadPromedio(votaron))
+
+
+console.log("-----3----")
+const votemenor30 =(votaron) =>{
+    const votaron30 = votaron.reduce( (accum,persona) => { 
+              return persona.age <30
+                     ?[...accum, persona]        
+                     :accum 
+     },[])
+     return votaron30
+ }
+ console.log(votemenor30(votaron))
+//let votaron=vote(arrayPersons)
+
+
+console.log("-----4----")
+//la edad promedio de los no votantes
+const noVote =(arrayPersons) =>{
+
+    const novotaron = arrayPersons.reduce( (accum,persona) => { 
+              return !persona.voted  
+                     ?[...accum, persona]        
+                    // ?accum + persona.age 
+                     :accum 
+     },[])
+     return novotaron
+ }
+  //console.log(noVote(arrayPersons))
+ let novotaron=noVote(arrayPersons)
+
+ const edadPromedioNoVota =(arrayPersons) =>{    
+    const sumaEdad = arrayPersons.reduce( (accum,persona) => { 
+            return accum + persona.age      
+    },0)
+    return sumaEdad/arrayPersons.length
+}
+console.log(edadPromedioNoVota(novotaron))
+
+console.log("-----5----")
+
+const porcentajevote =(arrayPersons) =>{
+    const votaron = arrayPersons.reduce( (accum,persona) => { 
+              return persona.voted  
+                     ? [...accum, persona]   
+                     :accum 
+     },[])
+     return (votaron.length*100)/arrayPersons.length
+ }
+ console.log(porcentajevote(arrayPersons))
+
+ console.log("-----6----")
+
+ const porcentajeNoVote =(arrayPersons) =>{
+     const noVotaron = arrayPersons.reduce( (accum,persona) => { 
+               return !persona.voted  
+                      ? [...accum, persona]   
+                      :accum 
+      },[])
+      return (noVotaron.length*100)/arrayPersons.length
+  }
+  console.log(porcentajeNoVote(arrayPersons))
+
+  console.log("-----7----")
+  //- el procentaje de mujeres que votaron
+  const womanVote =(arrayPersons) =>{
+     const votaron = arrayPersons.reduce( (accum,persona) => { 
+               return persona.voted && persona.gender === "mujer" 
+                      ?[...accum, persona]        
+                      :accum 
+      },[])
+      return (votaron.length *100)/arrayPersons.length
+  }
+  console.log(womanVote(arrayPersons))
+
+  console.log("-----8----")
+  //- el porcentaje de los hombres que votaron
+  const manVote =(arrayPersons) =>{
+    const votaron = arrayPersons.reduce( (accum,persona) => { 
+              return persona.voted && persona.gender === "hombre" 
+                     ?[...accum, persona]        
+                     :accum 
+     },[])
+     return (votaron.length *100)/arrayPersons.length
+ }
+ console.log(manVote(arrayPersons))
+ 
+ //dinamic porcentaje
+ const genderPorcen =(arrayPersons,gender) =>{
+    const votaron = arrayPersons.reduce( (accum,persona) => { 
+              return persona.voted && persona.gender === gender
+                     ?[...accum, persona]        
+                     :accum 
+     },[])
+     return (votaron.length *100)/arrayPersons.length
+ }
+ console.log(genderPorcen(arrayPersons,"mujer"))
